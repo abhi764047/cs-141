1f172d93c41a04ed525203c40fb20dc513478cb8
abhi764047 <42183639+abhi764047@users.noreply.github.com>
2018-08-16 20:57:36 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,41 @@
+//First Include the library
+#include<iostream>
+                                                                                        
+ using namespace std;
+
+//Write the main function
+int main()
+{
+/*Declaration of differnent data types by assigning different values to them including 
+ And finding the size of thr respective  data types*/
+
+
+
+ int a;
+ a= 8;
+ cout <<"The value of a is " <<a <<endl;
+ cout <<"The size of" <<sizeof(a) << endl;
+
+ float b;
+ b= 14.36;
+ cout <<"The value of b is " <<b <<endl;
+ cout <<"The size of" <<sizeof(b) << endl;
+
+ double c;
+ c= 16.3897;
+ cout <<"The value of c is " <<c <<endl;
+ cout <<"The size of" <<sizeof(c) <<endl;
+ 
+ char d;
+ d='&';
+ cout <<"The value of d is " <<d <<endl;
+ cout <<"The size of" <<sizeof(d) <<endl;
+ 
+ bool e;
+ e= (b==c);
+ cout <<"The value of e is " <<e <<endl;
+ cout <<"The size of" <<sizeof(e) <<endl;
+}
+
+ //the programs end here.
+
