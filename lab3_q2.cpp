65821b11747eb3914eca2be9e519a4c2ec09ad1d
abhi764047 <42183639+abhi764047@users.noreply.github.com>
2018-08-18 23:27:51 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main (){
+     int f = 40 + 50;
+     cout << f <<endl;
+int g = 40 - 50;
+     cout << g <<endl;
+int h = 40 * 50;
+     cout << h <<endl;
+int i = 180 / 30;
+     cout << i <<endl;
+int j = 160 % 30;
+     cout << j <<endl;
+
+  return 0;
+}
+//programs end here.
+
