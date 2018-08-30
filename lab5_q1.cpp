90c06385a52118a19e5825ef9913c436674cdc15
abhi764047 <42183639+abhi764047@users.noreply.github.com>
2018-08-30 19:18:53 +0530

    Add files via upload

---
+++ lab5_q1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //define the number
+  int a,b;
+  //print the number
+  cout<<"enter the value of a<<b"<<endl;
+  //give value to the numbers
+  cin>>a>>b;
+  //condition
+  if(a>b){
+  //if condition is true then print
+  cout<<"a is greater than b"<<endl;
+  }
+  //condition
+  if (b>a){
+  //print if condition is true
+  cout<<"b is greater than a"<<endl;
+  }
+  return 0;
+  }
