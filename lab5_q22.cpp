06d304fcfd15bfe0f2334e065172a7d2a9d472b8
abhi764047 <42183639+abhi764047@users.noreply.github.com>
2018-08-31 20:36:44 +0530

    Add files via upload

---
+++ lab5_q22.cpp
@@ -0,0 +1,20 @@
+// Write a C++ program to print all natural numbers from 1 to n. - using while loop
+#include<iostream>
+using namespace std;
+int main(){
+      // to print all natural numbers in 1 to n
+      // declaration of variables
+      // assigning value to it
+      // using while loop for the problems
+        int a;
+            int i=1;
+             cout << "enter number = ";
+              cin >> a;
+               while(i<=a)
+              {
+             cout <<i<< endl;
+          i++;
+         }
+        return 0;
+       }
+
---
+++ lab5_q23.cpp
@@ -0,0 +1,19 @@
+// Write a C++ program to print all natural numbers in reverse (from n to 1). - using while loop
+#include<iostream>
+using namespace std;
+int main(){
+    // to print all natural  number in reverse n to 1
+    // declaration of variables 
+    // assigning value to it
+    // using while loop for the problem
+                   int a;
+                   cout << " enter number = ";
+                    cin >> a;
+                   int  i=a;
+                        while(i!=0)
+                                  {
+                                  cout << i <<endl;
+                                            i--;
+                                  }
+   return 0;
+  }
