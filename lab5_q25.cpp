42a3b16c8bfe9e3273a7c018bd55003d06ed7a11
abhi764047 <42183639+abhi764047@users.noreply.github.com>
2018-09-02 19:53:54 +0530

    Add files via upload

---
+++ lab5_q25.cpp
@@ -0,0 +1,19 @@
+//Write a C++ program to print all even numbers between 1 to 100. - using while loop
+#include<iostream>
+using namespace std;
+int main(){
+    // to find the even numbers between 1 to 100
+    // declaration of variables
+    // assigning values to it
+    // using while loop for the problems
+       int a;
+             int i=2;
+                 cout <<'even numbers between 1 to 100 are'<<endl;
+                 a=100;
+                 while(i<a)
+                 {
+                   cout <<i<< endl;
+                   i=i+2;
+                  }
+      return 0;
+}
