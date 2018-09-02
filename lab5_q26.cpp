d77e479fc1e1a0a1eaad48e122b9955507e7643d
abhi764047 <42183639+abhi764047@users.noreply.github.com>
2018-09-02 20:08:10 +0530

    Add files via upload

---
+++ lab5_q26.cpp
@@ -0,0 +1,19 @@
+//Write a C++ program to print all odd number between 1 to 100.
+#include<iostream>
+using namespace std;
+int main(){
+    //to find the odd numbers between 1 to 100
+    //declaration of variables
+    //assigning value to it
+    //using while loop for the problems
+      int a;
+           int i=3;
+               cout << 'odd numbers between 1 to 100 are'<<endl;
+               a=100;
+               while(i<a)
+               {
+                cout <<i<< endl;
+                i=i+2;
+                }
+    return 0;
+  }
